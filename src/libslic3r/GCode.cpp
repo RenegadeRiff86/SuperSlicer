778d7d5d9223493a053d2efdacb08aa797aa58a9
Ted van den Brink <ted@digistate.nl>
2026-02-06 11:40:01 +0100

    Fix debug build
    (cherry picked from commit 1b3e0d2a662fcfb54da5fce50d1cfaf8a8df0b47)

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -7646,7 +7646,9 @@ std::string GCodeGenerator::_before_extrude(const ExtrusionPath &path, const std
     // compensate retraction
     if (m_delayed_layer_change.empty()) {
         gcode += m_writer.unlift();//this->unretract();
+#ifdef _DEBUGINFO
         assert(is_approx(m_writer.get_position().z(), m_layer->print_z, EPSILON) || m_loop_vase_mode);
+#endif
     } else {
         //check if an unlift happens
         std::string unlift = m_writer.unlift();
@@ -8208,7 +8210,9 @@ void GCodeGenerator::write_travel_to(std::string &gcode, Polyline& travel, std::
         this->writer().set_lift(this->writer().get_position().z() - *m_new_z_target);
         m_new_z_target.reset();
     }
+#ifdef _DEBUGINFO
     assert(!m_layer || is_approx(this->writer().get_unlifted_position().z(), m_layer->print_z, EPSILON) || comment == "Travel to a Wipe Tower" || m_loop_vase_mode);
+#endif
 }
 
 // generate a travel in xyz
